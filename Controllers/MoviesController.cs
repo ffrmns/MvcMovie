261095db2ba88298c97bc68d09098ed73486a3f0
ffrmns <fikrifa0@gmail.com>
2022-06-17 16:59:51 +0700

    Search in query parameter

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,10 +19,17 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
+              var movies = from m in _context.Movie
+                           select m;
+
+              if (!String.IsNullOrEmpty(searchString))
+              {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+              }
               return _context.Movie != null ? 
-                          View(await _context.Movie.ToListAsync()) :
+                          View(await movies.ToListAsync()) :
                           Problem("Entity set 'MvcMovieContext.Movie'  is null.");
         }
 
