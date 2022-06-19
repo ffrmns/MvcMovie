4929d979ff2ec672b8c28b9f65ab41ae1256b73b
ffrmns <fikrifa0@gmail.com>
2022-06-19 09:03:52 +0700

    Specify search request to HTTP GET

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -33,13 +33,6 @@ namespace MvcMovie.Controllers
                           Problem("Entity set 'MvcMovieContext.Movie'  is null.");
         }
 
-        // POST: Movies
-        [HttpPost]
-        public string Index(string searchString, bool notUsed)
-        {
-          return "From [HttpPost]Index: filter on " + searchString;
-        }
-
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
