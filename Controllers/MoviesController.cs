0874721b4b74033270f274dbef6aae96f6deeb8c
ffrmns <fikrifa0@gmail.com>
2022-06-19 10:31:09 +0700

    Add search by genre

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,17 +19,30 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+              IQueryable<string> genreQuery = from m in _context.Movie
+                                              orderby m.Genre
+                                              select m.Genre;
               var movies = from m in _context.Movie
                            select m;
 
-              if (!String.IsNullOrEmpty(searchString))
+              if (!string.IsNullOrEmpty(searchString))
               {
                 movies = movies.Where(s => s.Title!.Contains(searchString));
               }
+              if (!string.IsNullOrEmpty(movieGenre))
+              {
+                movies = movies.Where(x => x.Genre == movieGenre);
+              }
+
+              var movieGenreVM = new MovieGenreViewModel
+              {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+              };
               return _context.Movie != null ? 
-                          View(await movies.ToListAsync()) :
+                          View(movieGenreVM) :
                           Problem("Entity set 'MvcMovieContext.Movie'  is null.");
         }
 
